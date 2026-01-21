afc8a3a671737e29c3ed61b5d8972af045904347
Danilo Humberto <danilohumberto12@hotmail.com>
2026-01-21 15:53:26 -0300

    feat: global pipe to get error messages where came from dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        const formatted = errors.reduce(
+          (acc, err) => {
+            const firstMsg = err.constraints
+              ? Object.values(err.constraints)[0]
+              : 'Valor inválido.';
+
+            acc[err.property] = firstMsg;
+            return acc;
+          },
+          {} as Record<string, string>,
+        );
+
+        return new BadRequestException({
+          message: 'Dados inválidos.',
+          errors: formatted,
+        });
+      },
+    }),
+  );
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
